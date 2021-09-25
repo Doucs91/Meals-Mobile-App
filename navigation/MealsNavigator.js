960447ae108349d1a2118e302b923f580e8fd985
Sady Doucoure <sadydoucoure@gmail.com>
2021-09-25 16:07:56 -0400

    Finxing imports typo

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,24 +1,28 @@
+import { Platform } from "react-native";
+import Colors from "../constants/Colors";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealScreen from "../screens/CategoryMealsScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
-import MealDetail from "../screens/MealDetailScreen";
 
-const MealsNavigator = createStackNavigator({
-  Categories: CategoriesScreen,
-  CategoryMeals: {
-    screen: CategoryMealScreen,
-  },
-  MealDetail: MealDetailScreen,
-},
-{
-  defaultNavigationOptions:{
-    headerStyle: {
-      backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
+const MealsNavigator = createStackNavigator(
+  {
+    Categories: CategoriesScreen,
+    CategoryMeals: {
+      screen: CategoryMealScreen,
     },
-    headerTintColor: Platform.OS === "android" ? "white" : Colors.primaryColor,
+    MealDetail: MealDetailScreen,
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
+      },
+      headerTintColor:
+        Platform.OS === "android" ? "white" : Colors.primaryColor,
+    },
   }
-});
+);
 
 export default createAppContainer(MealsNavigator);
